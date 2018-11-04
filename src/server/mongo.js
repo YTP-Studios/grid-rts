a7813b70cc8ebe5084750349526c31141479d939
dannykong12 <dannykong12@gmail.com>
2018-11-03 23:44:52 -0400

    include mongo database

---
+++ src/server/mongo.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+mongoose.connect();
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+});
+
+userSchema.methods.authenticate = function (pass) {
+  return pass === this.password;
+}
+
+var user = mongoose.model('User', userSchema);
+
+
